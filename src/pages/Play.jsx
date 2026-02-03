a742abb21beed3a11e4c3bb62bcec44e6db9c712
Alexandre Armando <armando.alexandrej@gmail.com>
2026-02-02 20:18:59 -0500

    update scoreboard styling

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,6 +1,16 @@
-const Play = () => {
-  return;
-  <div></div>;
+const Play = ({ leftScore, rightScore, round }) => {
+  return (
+    <div className="scoreboard">
+      <div className="score score-left">{leftScore}</div>
+
+      <div className="round-box">
+        <span className="round-text">ROUND {round}</span>
+        <span className="round-hebrew">1 thess 5:21</span>
+      </div>
+
+      <div className="score score-right">{rightScore}</div>
+    </div>
+  );
 };
 
 export default Play;
